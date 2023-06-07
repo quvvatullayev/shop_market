465868e957ae907cba304d2f1a4d1f07fe0bdb0f
ogabek <quvvatullayev@gmail.com>
2023-06-07 19:16:18 +0500

    create serializers model

---
+++ shop/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import (
+    User,
+    Category,
+    Sub_category,
+    Product,
+    Cart,
+    Order,
+)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class Sub_categorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sub_category
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
