23c4e127735655b8da56d12455fff8faf376f4b4
ogabek <quvvatullayev@gmail.com>
2023-06-08 10:03:01 +0500

    fix cart

--- shop/views/cart.py
+++ shop/views/cart.py
@@ -3,32 +3,32 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
 from ..models import (
-    Product,
+    Cart,
 )
 from ..serializers import (
-    ProductSerializer,
+    CartSerializer,
 )
 
 class AddCart(APIView):
     def post(self, request: Request):
-        serializer = ProductSerializer(data=request.data)
+        serializer = CartSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({
                 'status': True,
-                'message': 'Product created successfully',
+                'message': 'Product added successfully',
                 'data': serializer.data
             }, status=status.HTTP_201_CREATED)
         return Response({
             'status': False,
-            'message': 'Product not created',
+            'message': 'Product not added',
             'data': serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
-    
+
 class CartList(APIView):
     def get(self, request: Request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
+        carts = Cart.objects.all()
+        serializer = CartSerializer(carts, many=True)
         return Response({
             'status': True,
             'message': 'Product list',
@@ -37,40 +37,34 @@ class CartList(APIView):
     
 class GetCart(APIView):
     def get(self, request: Request, sub_category_id):
-        product = Product.objects.get(sub_category=sub_category_id)
-        serializer = ProductSerializer(product)
+        carts = Cart.objects.filter(sub_category=sub_category_id)
+        serializer = CartSerializer(carts, many=True)
         return Response({
             'status': True,
-            'message': 'Product',
+            'message': 'Product list',
             'data': serializer.data
         }, status=status.HTTP_200_OK)
     
 class UpdateCart(APIView):
     def post(self, request: Request):
         data = request.data
-        product = Product.objects.get(id=data['id'])
-        serializer = ProductSerializer(product, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                'status': True,
-                'message': 'Product updated successfully',
-                'data': serializer.data
-            }, status=status.HTTP_200_OK)
+        cart = Cart.objects.get(id=data['id'])
+        cart.quantity = data.get('quantity', cart.quantity)
+        cart.save()
+        serializer = CartSerializer(cart)
         return Response({
-            'status': False,
-            'message': 'Product not updated',
-            'data': serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
+            'status': True,
+            'message': 'Product updated successfully',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
     
 class DeleteCart(APIView):
     def post(self, request: Request):
         data = request.data
-        product = Product.objects.get(id=data['id'])
-        product.delete()
+        cart = Cart.objects.get(id=data['id'])
+        cart.delete()
         return Response({
             'status': True,
             'message': 'Product deleted successfully',
-            'data': []
         }, status=status.HTTP_200_OK)
     
