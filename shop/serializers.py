9f44af7073c6e29194ca89dda8f2b32e6a75f47e
ogabek <quvvatullayev@gmail.com>
2023-06-08 08:40:17 +0500

    add relations ships

--- shop/serializers.py
+++ shop/serializers.py
@@ -14,19 +14,23 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CategorySerializer(serializers.ModelSerializer):
+    # sub_category = serializers.SerializerMethodField()
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'discription', 'image']
 
 class Sub_categorySerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=False, read_only=True)
     class Meta:
         model = Sub_category
-        fields = '__all__'
+        fields = ['id', 'name', 'discription', 'image', 'category']
 
 class ProductSerializer(serializers.ModelSerializer):
+    # category = CategorySerializer()
+    sub_category = Sub_categorySerializer(many=False, read_only=True)
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'discription', 'image', 'price', 'sub_category']
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
