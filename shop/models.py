c718891390da3264b9a3f3f1e2a7eb61c1250247
ogabek <quvvatullayev@gmail.com>
2023-06-12 16:52:05 +0500

    fix

--- shop/models.py
+++ shop/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     chat_id = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
 
     def __str__(self):
         return self.username
